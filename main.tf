faea69d5a59087945e4fe02b3b6446608f745f7a
ArsalanSan <117136365+ArsalanSan@users.noreply.github.com>
2023-04-28 12:49:19 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 ####----------------------------
 
 module "vpc" {
-  source  = "git::https://github.com/ArsalanSan/networks.git?ref=main"
+  source  = "git::https://github.com/ArsalanSan/networks.git"
   name    = var.name
   subnets = var.subnets 
 }
