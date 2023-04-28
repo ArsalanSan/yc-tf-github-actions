6e5bb8f40280acb34112dbc4f2fafbe8273c5afa
ArsalanSan <arstomorrow@gmail.com>
2023-04-28 22:29:40 +0800

    pull request

--- variables.tf
+++ variables.tf
@@ -38,8 +38,8 @@ variable "subnets" {
   )
   default = [
     { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
-#    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
-#    { zone = "ru-central1-c", cidr = "10.0.3.0/24" },
+    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
+    { zone = "ru-central1-c", cidr = "10.0.3.0/24" },
   ]
 }
 
